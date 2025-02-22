01532229195fcefd922054930029ab4a2ee290ea
vaishnavi-s-murukan <vaishnavismurukan@gmail.com>
2025-02-22 15:03:53 +0530

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import { config } from "dotenv";
 config(); // Load before anything else
 
 import express from "express";
-import morgan from "morgan";
+//import morgan from "morgan";
 import appRouter from "./routes/index.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -14,8 +14,6 @@ app.use(cors({origin:"http://localhost:5173",credentials: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-//remove it in production
-app.use(morgan("dev"));
 
 app.use("/api/v1",appRouter);
 
