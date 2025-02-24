389c87c514dd26163ce7a52f024e5802fffd7e3e
vaishnavi-s-murukan <vaishnavismurukan@gmail.com>
2025-02-25 00:52:00 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,7 @@ config();
 
 const app = express();
 //middlewares
-app.use(cors({origin:"https://ai-mern-gpt.web.app",credentials: true}));
+app.use(cors({origin:"http://localhost:5000",credentials: true}));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
